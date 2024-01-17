c208feaf1b1b554defa92878ba8723e6319456bc
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2024-01-17 23:55:56 +0200

    Adding levels and displaying answers and score

--- proiect.cs
+++ proiect.cs
@@ -1,14 +1,44 @@
-using System;
 using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+public enum Level
+{
+    Beginner,
+    Intermediate,
+    Advanced
+}
 
 public class Question
 {
     public string Text { get; set; }
     public List<string> Options { get; set; }
     public int CorrectOption { get; set; }
+    public Level Level { get; set; }
+}
+
+public class BeginnerQuestion : Question
+{
+    public BeginnerQuestion()
+    {
+        Level = Level.Beginner;
+    }
+}
+
+public class IntermediateQuestion : Question
+{
+    public IntermediateQuestion()
+    {
+        Level = Level.Intermediate;
+    }
+}
+
+public class AdvancedQuestion : Question
+{
+    public AdvancedQuestion()
+    {
+        Level = Level.Advanced;
+    }
 }
 
 public class Quiz
@@ -25,9 +55,9 @@ public class Quiz
         questions.Add(question);
     }
 
-    public IEnumerable<Question> GetAllQuestions()
+    public IEnumerable<Question> GetQuestionsByLevel(Level level)
     {
-        return questions;
+        return questions.Where(q => q.Level == level);
     }
 }
 
@@ -39,43 +69,64 @@ class Program
 
         try
         {
-            quiz.AddQuestion(new Question { Text = "1+1", Options = new List<string> { "1", "2", "3" }, CorrectOption = 2 });
+            quiz.AddQuestion(new BeginnerQuestion { Text = "1+1", Options = new List<string> { "1", "2", "3" }, CorrectOption = 2 });
+            quiz.AddQuestion(new IntermediateQuestion { Text = "2*2", Options = new List<string> { "2", "4", "3" }, CorrectOption = 2 });
+            quiz.AddQuestion(new AdvancedQuestion { Text = "5*5", Options = new List<string> { "10", "2", "25" }, CorrectOption = 3 });
 
-            var questions = quiz.GetAllQuestions();
+            Console.WriteLine("Alegeți un nivel: a) Începător, b) Mediu, c) Avansat");
+            char userLevel = Convert.ToChar(Console.ReadLine().ToLower());
+
+            Level level = Level.Beginner;
+            switch (userLevel)
+            {
+                case 'a':
+                    level = Level.Beginner;
+                    break;
+                case 'b':
+                    level = Level.Intermediate;
+                    break;
+                case 'c':
+                    level = Level.Advanced;
+                    break;
+                default:
+                    Console.WriteLine("Opțiune invalidă.");
+                    return;
+            }
+
+            var questions = quiz.GetQuestionsByLevel(level);
 
             int correctAnswers = 0;
 
             foreach (var question in questions)
             {
-                bool correct = false;
-                while (!correct)
+                Console.WriteLine(question.Text);
+                for (int i = 0; i < question.Options.Count; i++)
                 {
-                    Console.WriteLine(question.Text);
-                    for (int i = 0; i < question.Options.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {question.Options[i]}");
-                    }
+                    Console.WriteLine($"{i + 1}. {question.Options[i]}");
+                }
 
-                    Console.Write("Alege o optiune: ");
-                    int userOption = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Alegeti o optiune:");
+                int userOption = Convert.ToInt32(Console.ReadLine());
 
-                    if (userOption == question.CorrectOption)
-                    {
-                        Console.WriteLine("Corect! ");
-                        correctAnswers++;
-                        correct = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Gresit Vericule !!!");
-                        Console.WriteLine($"raspunsul corest este: {question.Options[question.CorrectOption - 1]}");
-                    }
+                if (userOption - 1 == question.CorrectOption - 1)
+                {
+                    Console.WriteLine("Corect!");
+                    correctAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorect!");
+                    Console.WriteLine($"Raspusnul corest corect era: {question.Options[question.CorrectOption - 1]}");
                 }
             }
+
+            Console.WriteLine($"A-ti raspusn corect la {correctAnswers} intrebari");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("eroare " + ex.Message);
+            Console.WriteLine($"A aparut o eroare: {ex.Message}");
         }
+        Console.WriteLine("....");
+        Console.ReadLine(); 
     }
 }
