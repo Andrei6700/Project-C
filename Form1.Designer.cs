e892b66ed1835a5130c2a42ef8afd14bbbfbd9d3
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2024-01-24 19:18:31 +0200

    update major

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+using System.Drawing;
+
+namespace Proiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label TITLE;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Qwestion = new System.Windows.Forms.Label();
+            this.btn_A = new System.Windows.Forms.Button();
+            this.btn_B = new System.Windows.Forms.Button();
+            this.btn_C = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.answer_label = new System.Windows.Forms.Label();
+            this.Arms = new System.Windows.Forms.RadioButton();
+            this.Shoulders = new System.Windows.Forms.RadioButton();
+            this.Legs = new System.Windows.Forms.RadioButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Chest = new System.Windows.Forms.RadioButton();
+            this.Back = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            TITLE = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TITLE
+            // 
+            TITLE.Font = new System.Drawing.Font("Myanmar Text", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            TITLE.ForeColor = System.Drawing.Color.Black;
+            TITLE.Location = new System.Drawing.Point(104, 12);
+            TITLE.Name = "TITLE";
+            TITLE.Size = new System.Drawing.Size(531, 61);
+            TITLE.TabIndex = 9;
+            TITLE.Text = "GYM EDITION";
+            TITLE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            TITLE.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Qwestion
+            // 
+            this.Qwestion.Location = new System.Drawing.Point(193, 119);
+            this.Qwestion.Name = "Qwestion";
+            this.Qwestion.Size = new System.Drawing.Size(385, 45);
+            this.Qwestion.TabIndex = 0;
+            this.Qwestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Qwestion.Click += new System.EventHandler(this.Qwestion_Click);
+            // 
+            // btn_A
+            // 
+            this.btn_A.Location = new System.Drawing.Point(269, 180);
+            this.btn_A.Name = "btn_A";
+            this.btn_A.Size = new System.Drawing.Size(222, 45);
+            this.btn_A.TabIndex = 1;
+            this.btn_A.UseVisualStyleBackColor = true;
+            this.btn_A.Click += new System.EventHandler(this.btn_A_Click);
+            // 
+            // btn_B
+            // 
+            this.btn_B.Location = new System.Drawing.Point(269, 242);
+            this.btn_B.Name = "btn_B";
+            this.btn_B.Size = new System.Drawing.Size(222, 45);
+            this.btn_B.TabIndex = 2;
+            this.btn_B.UseVisualStyleBackColor = true;
+            this.btn_B.Click += new System.EventHandler(this.btn_B_Click);
+            // 
+            // btn_C
+            // 
+            this.btn_C.Location = new System.Drawing.Point(269, 300);
+            this.btn_C.Name = "btn_C";
+            this.btn_C.Size = new System.Drawing.Size(222, 45);
+            this.btn_C.TabIndex = 3;
+            this.btn_C.UseVisualStyleBackColor = true;
+            this.btn_C.Click += new System.EventHandler(this.btn_C_Click);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(354, 396);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next.TabIndex = 4;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // answer_label
+            // 
+            this.answer_label.BackColor = System.Drawing.SystemColors.Control;
+            this.answer_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.answer_label.Location = new System.Drawing.Point(211, 361);
+            this.answer_label.Name = "answer_label";
+            this.answer_label.Size = new System.Drawing.Size(329, 23);
+            this.answer_label.TabIndex = 5;
+            this.answer_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.answer_label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Arms
+            // 
+            this.Arms.AutoSize = true;
+            this.Arms.Location = new System.Drawing.Point(37, 170);
+            this.Arms.Name = "Arms";
+            this.Arms.Size = new System.Drawing.Size(48, 17);
+            this.Arms.TabIndex = 6;
+            this.Arms.TabStop = true;
+            this.Arms.Text = "Arms";
+            this.Arms.UseVisualStyleBackColor = true;
+            this.Arms.CheckedChanged += new System.EventHandler(this.Arms_CheckedChanged);
+            // 
+            // Shoulders
+            // 
+            this.Shoulders.AutoSize = true;
+            this.Shoulders.Location = new System.Drawing.Point(37, 194);
+            this.Shoulders.Name = "Shoulders";
+            this.Shoulders.Size = new System.Drawing.Size(72, 17);
+            this.Shoulders.TabIndex = 7;
+            this.Shoulders.TabStop = true;
+            this.Shoulders.Text = "Shoulders";
+            this.Shoulders.UseVisualStyleBackColor = true;
+            this.Shoulders.CheckedChanged += new System.EventHandler(this.Shoulders_CheckedChanged);
+            // 
+            // Legs
+            // 
+            this.Legs.AutoSize = true;
+            this.Legs.Location = new System.Drawing.Point(37, 218);
+            this.Legs.Name = "Legs";
+            this.Legs.Size = new System.Drawing.Size(48, 17);
+            this.Legs.TabIndex = 8;
+            this.Legs.TabStop = true;
+            this.Legs.Text = "Legs";
+            this.Legs.UseVisualStyleBackColor = true;
+            this.Legs.CheckedChanged += new System.EventHandler(this.Legs_CheckedChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Chest
+            // 
+            this.Chest.AutoSize = true;
+            this.Chest.Location = new System.Drawing.Point(37, 242);
+            this.Chest.Name = "Chest";
+            this.Chest.Size = new System.Drawing.Size(52, 17);
+            this.Chest.TabIndex = 11;
+            this.Chest.TabStop = true;
+            this.Chest.Text = "Chest";
+            this.Chest.UseVisualStyleBackColor = true;
+            this.Chest.CheckedChanged += new System.EventHandler(this.Chest_CheckedChanged);
+            // 
+            // Back
+            // 
+            this.Back.AutoSize = true;
+            this.Back.Location = new System.Drawing.Point(37, 266);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(50, 17);
+            this.Back.TabIndex = 12;
+            this.Back.TabStop = true;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.CheckedChanged += new System.EventHandler(this.Back_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proiect.Properties.Resources.sam_sulek;
+            this.pictureBox1.Location = new System.Drawing.Point(535, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(185, 141);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Chest);
+            this.Controls.Add(TITLE);
+            this.Controls.Add(this.Legs);
+            this.Controls.Add(this.Shoulders);
+            this.Controls.Add(this.Arms);
+            this.Controls.Add(this.answer_label);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.btn_C);
+            this.Controls.Add(this.btn_B);
+            this.Controls.Add(this.btn_A);
+            this.Controls.Add(this.Qwestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Qwestion;
+        private System.Windows.Forms.Button btn_A;
+        private System.Windows.Forms.Button btn_B;
+        private System.Windows.Forms.Button btn_C;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Label answer_label;
+        private System.Windows.Forms.RadioButton Arms;
+        private System.Windows.Forms.RadioButton Shoulders;
+        private System.Windows.Forms.RadioButton Legs;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RadioButton Chest;
+        private System.Windows.Forms.RadioButton Back;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect
+{
+    public partial class Form1 : Form
+    {
+        private class Question
+        {
+            public string Text { get; set; }
+            public List<string> Options { get; set; }
+            public int CorrectOptionIndex { get; set; }
+        }
+
+
+        // intrebarile pentru brand
+        private List<Question> ArmsQuestions = new List<Question>
+{
+    new Question
+    {
+        Text = "Care este cel mai bun exercitiu pentru antebrat",
+        Options = new List<string> { "Sam Sulek ", "Chin ups", "Hammer curls" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care este cel mai bun exercitiu pentru biceps?",
+        Options = new List<string> { "Hammer curls", "Tricep dips", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care este cel mai bun exercitiu pentru triceps?",
+        Options = new List<string> { "Bicep curls", "Tricep dips", "Leg press" },
+        CorrectOptionIndex = 1
+    },
+    new Question
+    {
+        Text = "Care exercitiu nu este benefic pentru brate?",
+        Options = new List<string> { "Bicep curls", "Tricep dips", "Leg press" },
+        CorrectOptionIndex = 2
+    },
+    new Question
+    {
+        Text = "Care exercitiu lucreaza cel mai mult bratele?",
+        Options = new List<string> { "Bicep curls", "Tricep dips", "Chin ups" },
+        CorrectOptionIndex = 2
+    },
+};
+
+        // intrebarile pentru shoulders
+        private List<Question> ShouldersQuestions = new List<Question>
+        {
+              new Question
+    {
+        Text = "Care este cel mai bun exercitiu pentru deltoidul posterior ?",
+        Options = new List<string> { "Face Pull", "Military Press", "Lateral Raises" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Cel mai prost exercitiu pentru umeri ?",
+        Options = new List<string> { "Upright Rows", "Military Press", "Lateral Raises" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Ce unghi trebuie sa aibe bratele cand faci militari press ?",
+        Options = new List<string> { "90 grade", "45 grade", "30 grade" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu targeteaza deltoidul medial ?",
+        Options = new List<string> { "Lateral Raises", "Military Press", "Upright Rows" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu nu targeteaza deltoidul frontal",
+        Options = new List<string> { "Rear Delt Flyes", "Military Press", "Front Raises" },
+        CorrectOptionIndex = 0
+    },
+};
+
+        // intrebarile pentru picioare
+        private List<Question> LegsQuestions = new List<Question>
+{
+    new Question
+    {
+        Text = "Cel mai bun exercitiu pentru Cvadriceps ?",
+        Options = new List<string> { "Front Squats", "Hammer curls", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Cel mai bun exercitiu pentru gambe",
+        Options = new List<string> { "Ridicari pe varfuri", "leg press", "Ridicari pe varfuri din sezut ?" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Cel mai prost exercitiu pentru bicepsul femural ?",
+        Options = new List<string> { "Leg extensions", "Deadlifts", "Lunges" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu nu lucreaza cvadricepsul?",
+        Options = new List<string> { "Leg curls", "Squats", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu lucreaza cel mai mult fesierii?",
+        Options = new List<string> { "Hip thrusts", "Squats", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+};
+
+        // intrebarile pentru pieptan
+        private List<Question> ChestQuestions = new List<Question>
+{
+    new Question
+    {
+        Text = "Ce exercitiu este bun pentru forta la piept?",
+        Options = new List<string> { "Bench Press", "Bicep Curls", "Leg Press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu lucreaza partea superioara a pieptului?",
+        Options = new List<string> { "Bicep Curls", "Incline Bench Press", "Leg Press" },
+        CorrectOptionIndex = 1
+    },
+    new Question
+    {
+        Text = "Care exercitiu nu este benefic pentru piept?",
+        Options = new List<string> { "Bench Press", "Bicep Curls", "Leg Press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Care exercitiu lucreaza partea inferioara a pieptului?",
+        Options = new List<string> { "Bicep Curls", "Leg Press", "Decline Bench Press" },
+        CorrectOptionIndex = 2
+    },
+    new Question
+    {
+        Text = "Care exercitiu lucreaza intregul piept?",
+        Options = new List<string> { "Bicep Curls", "Dumbbell Press", "Leg Press" },
+        CorrectOptionIndex = 1
+    },
+};
+
+        // intrebarile pentru spate
+        private List<Question> BackQuestions = new List<Question>
+{
+    new Question
+    {
+        Text = "Care exercitiu activeaza cel mai bine marele dorsal ?",
+        Options = new List<string> { "Pull ups", "Bicep curls", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Ce exercitiu nu targeteaza trapezul ?",
+        Options = new List<string> { "Shrugs", "Deadlift", "Bicep curls" },
+        CorrectOptionIndex = 2
+    },
+    new Question
+    {
+        Text = "Care exercitiu targeteaza muschii romboizi ?",
+        Options = new List<string> { "Rows", "Bicep curls", "Leg press" },
+        CorrectOptionIndex = 0
+    },
+    new Question
+    {
+        Text = "Ce exercitiu nu targeteaza muschii spatelui ?",
+        Options = new List<string> { "Pull ups", "Deadlift", "Romanian Deadlift" },
+        CorrectOptionIndex = 2
+    },
+    new Question
+    {
+        Text = "Ce exercitiu targeteaza mai multe grupe musculare a spatelui ?",
+        Options = new List<string> { "Pull ups", "Deadlift", "Rows" },
+        CorrectOptionIndex = 1
+    },
+};
+
+
+        private List<Question> currentQuestions;
+        private int currentQuestionIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Setează nivelul implicit la începător și afișează întrebările
+            Arms.Checked = true;
+            currentQuestions = ArmsQuestions;
+            DisplayQuestion();
+        }
+
+        private void DisplayQuestion()
+        {
+            // Verifică dacă s-au terminat întrebările pentru nivelul curent
+            if (currentQuestionIndex < currentQuestions.Count)
+            {
+                // Afișează întrebarea și resetează label-ul pentru răspuns
+                Qwestion.Text = currentQuestions[currentQuestionIndex].Text;
+                answer_label.Text = "";
+
+                // Afișează butoanele pentru opțiuni
+                btn_A.Text = $" {currentQuestions[currentQuestionIndex].Options[0]}";
+                btn_B.Text = $" {currentQuestions[currentQuestionIndex].Options[1]}";
+                btn_C.Text = $" {currentQuestions[currentQuestionIndex].Options[2]}";
+            }
+            else
+            {
+                // Dacă s-au terminat întrebările, afișează un mesaj și resetează la prima întrebare
+                MessageBox.Show("Ai terminat întrebările pentru acest nivel!");
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+        }
+
+        // Evenimente pentru butoane și radio buttons
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Qwestion_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            // incrementare indexului pt urmatoarea intrebare
+            currentQuestionIndex++;
+            DisplayQuestion();
+        }
+
+        private void btn_A_Click(object sender, EventArgs e)
+        {
+            // verifica daca raspunsul e corect
+            if (currentQuestions[currentQuestionIndex].CorrectOptionIndex == 0)
+            {
+                // raspuns corect
+                answer_label.ForeColor = Color.Green; 
+                answer_label.Text = "Răspunsul corect!";
+            }
+            else
+            {
+                //rapsun gresit
+                answer_label.ForeColor = Color.Red; 
+                answer_label.Text = "Mai încearcă!";
+            }
+        }
+
+        private void btn_B_Click(object sender, EventArgs e)
+        {
+            // verifica daca raspunsul este corect
+            if (currentQuestions[currentQuestionIndex].CorrectOptionIndex == 1)
+            {
+                answer_label.ForeColor = Color.Green; 
+                answer_label.Text = "Răspunsul corect!";
+            }
+            else
+            {
+                answer_label.ForeColor = Color.Red; 
+                answer_label.Text = "Mai încearcă!";
+            }
+        }
+
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            // verifica daca raspunsul este corect
+            if (currentQuestions[currentQuestionIndex].CorrectOptionIndex == 2)
+            {
+                answer_label.ForeColor = Color.Green; 
+                answer_label.Text = "Răspunsul corect!";
+            }
+            else
+            {
+                answer_label.ForeColor = Color.Red; 
+                answer_label.Text = "Mai încearcă!";
+            }
+        }
+
+        private void Arms_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Arms.Checked)
+            {
+                currentQuestions = ArmsQuestions;
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+
+        }
+
+        private void Shoulders_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Shoulders.Checked)
+            {
+                currentQuestions = ShouldersQuestions;
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+        }
+
+        private void Legs_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Legs.Checked)
+            {
+                currentQuestions = LegsQuestions;
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+        }
+
+        //lvl back
+        private void Back_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Back.Checked)
+            {
+                currentQuestions = BackQuestions;
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+        }
+        //lvl chest
+        private void Chest_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chest.Checked)
+            {
+                currentQuestions = ChestQuestions;
+                currentQuestionIndex = 0;
+                DisplayQuestion();
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            // e titlul
+        }
+
+        //picture box 1
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //picture box 2
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
